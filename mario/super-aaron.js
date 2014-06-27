58de7bfec3757f68d73a4cb669eac118e52c2d21
Sumedha <pramods@umich.edu>
2014-06-27 02:12:06 -0700

    adding more dying functions

--- mario/super-aaron.js
+++ mario/super-aaron.js
@@ -97,13 +97,33 @@ Q.Sprite.extend("Enemy",{
     });
   }
 });
-        
+         
 //enemy that goes up and down
 Q.Sprite.extend("VerticalEnemy", {
     init: function(p) {
-        this._super(p, {vy: -100, rangeY: 200, gravity: 0 });
-        this.add("2d, commonEnemy");                
-        this.p.initialY = this.p.y;
+      this._super(p, { sheet: 'enemy', vx: 100 });
+
+      // Enemies use the Bounce AI to change direction 
+      // whenver they run into something.
+      this.add('2d, aiBounce');
+
+      // Listen for a sprite collision, if it's the player,
+      // end the game unless the enemy is hit on top
+      this.on("bump.left,bump.right,bump.bottom",function(collision) {
+        if(collision.obj.isA("Player")) { 
+          Q.stageScene("endGame",1, { label: "You Died" }); 
+          collision.obj.destroy();
+        }
+      });
+
+      // If the enemy gets hit on the top, destroy it
+      // and give the user a "hop"
+      this.on("bump.top",function(collision) {
+        if(collision.obj.isA("Player")) { 
+          this.destroy();
+          collision.obj.p.vy = -300;
+        }
+      });
     },
     step: function(dt) {                
         if(this.p.y - this.p.initialY >= this.p.rangeY && this.p.vy > 0) {
