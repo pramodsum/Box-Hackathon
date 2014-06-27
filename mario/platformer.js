e8277434015907fdc85ffb5c7a27f4b9b531f57c
Sumedha <pramods@umich.edu>
2014-06-26 23:50:01 -0700

    ergh

--- mario/platformer.js
+++ mario/platformer.js
@@ -10,8 +10,6 @@
 
 window.addEventListener("load",function() {
 
-document.getElementById("back").style.zIndex="1";
-
 // Set up an instance of the Quintus engine  and include
 // the Sprites, Scenes, Input and 2D module. The 2D module
 // includes the `TileLayer` class as well as the `2d` componet.
