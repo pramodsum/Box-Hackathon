f6c163adb5ffa5a3d87da7e0e780cf29deb5b228
Sumedha <pramods@umich.edu>
2014-06-27 03:29:43 -0700

    Revert ee5ca63..88257c4
    This rolls back to commit ee5ca63d7f2dffd6be11109b309f3fb344283af5.

--- cloud-runner/js/main.js
+++ cloud-runner/js/main.js
@@ -2,7 +2,7 @@
 enchant();
  
 var browserWidth = window.innerWidth || document.body.clientWidth;
-var browserHeight = window.innerHeight || document.body.clientHeight + 100;
+var browserHeight = window.innerHeight || document.body.clientHeight;
 window.onload = function() {
 
     // Starting point
