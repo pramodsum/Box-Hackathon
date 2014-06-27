6030944374f6de7c753996d181f6b948b142211d
Sumedha <pramods@umich.edu>
2014-06-27 02:13:43 -0700

    maybe?

--- mario/super-aaron.js
+++ mario/super-aaron.js
@@ -97,33 +97,31 @@ Q.Sprite.extend("Enemy",{
     });
   }
 });
-         
+        
 //enemy that goes up and down
 Q.Sprite.extend("VerticalEnemy", {
     init: function(p) {
-      this._super(p, { sheet: 'enemy', vx: 100 });
+        this._super(p, { sheet: 'enemy', vy: -100, rangeY: 200, gravity: 0  });
+        this.add("2d, aiBounce");                
+        this.p.initialY = this.p.y;
+        
+        // Listen for a sprite collision, if it's the player,
+        // end the game unless the enemy is hit on top
+        this.on("bump.left,bump.right,bump.bottom",function(collision) {
+          if(collision.obj.isA("Player")) { 
+            Q.stageScene("endGame",1, { label: "You Died" }); 
+            collision.obj.destroy();
+          }
+        });
 
-      // Enemies use the Bounce AI to change direction 
-      // whenver they run into something.
-      this.add('2d, aiBounce');
-
-      // Listen for a sprite collision, if it's the player,
-      // end the game unless the enemy is hit on top
-      this.on("bump.left,bump.right,bump.bottom",function(collision) {
-        if(collision.obj.isA("Player")) { 
-          Q.stageScene("endGame",1, { label: "You Died" }); 
-          collision.obj.destroy();
-        }
-      });
-
-      // If the enemy gets hit on the top, destroy it
-      // and give the user a "hop"
-      this.on("bump.top",function(collision) {
-        if(collision.obj.isA("Player")) { 
-          this.destroy();
-          collision.obj.p.vy = -300;
-        }
-      });
+        // If the enemy gets hit on the top, destroy it
+        // and give the user a "hop"
+        this.on("bump.top",function(collision) {
+          if(collision.obj.isA("Player")) { 
+            this.destroy();
+            collision.obj.p.vy = -300;
+          }
+        });
     },
     step: function(dt) {                
         if(this.p.y - this.p.initialY >= this.p.rangeY && this.p.vy > 0) {
