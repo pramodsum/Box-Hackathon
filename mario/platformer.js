684ca65eaea7d31f3831e7c2e3ac534b5e2c8180
Sumedha <pramods@umich.edu>
2014-06-26 23:48:37 -0700

    z index

--- mario/platformer.js
+++ mario/platformer.js
@@ -7,6 +7,8 @@
 // This is the example from the website homepage, it consists
 // a simple, non-animated platformer with some enemies and a 
 // target for the player.
+document.getElementById("back").style.zIndex="1000";
+
 window.addEventListener("load",function() {
 
 // Set up an instance of the Quintus engine  and include
