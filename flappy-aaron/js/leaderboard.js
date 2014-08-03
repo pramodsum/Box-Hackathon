3f9cb548fc4cbe8888b0784fa2e019647499bbd5
Sumedha Pramod <spramod@box.com>
2014-08-02 22:24:52 -0700

    attempts to fix old version leaderboard

--- flappy-aaron/js/leaderboard.js
+++ flappy-aaron/js/leaderboard.js
@@ -1,65 +1,56 @@
-var app = angular.module('leaderboard', ['firebase']);
+var LEADERBOARD_SIZE = 5;
 
-app.controller('MainCtrl', ['$scope', 'ContestantsService', '$firebase', function ($scope, ContestantsService, $firebase) {
+// Build some firebase references.
+var rootRef = new Firebase('https://example.firebaseio-demo.com/leaderboard');
+var scoreListRef = rootRef.child("scoreList");
+var highestScoreRef = rootRef.child("highestScore");
 
-    $scope.newContestant = { email: '', name: '', score: '' };
-    $scope.currentContestant = null;
+// Keep a mapping of firebase locations to HTML elements, so we can move / remove elements as necessary.
+var htmlForPath = {};
 
-    // Explicit
-    $scope.contestants = ContestantsService.getContestants();
+// Helper function that takes a new score snapshot and adds an appropriate row to our leaderboard table.
+function handleScoreAdded(scoreSnapshot, prevScoreName) {
+    var newScoreRow = $("<tr/>");
+    newScoreRow.append($("<td/>").append($("<em/>").text(scoreSnapshot.val().name)));
+    newScoreRow.append($("<td/>").text(scoreSnapshot.val().score));
 
-    $scope.addContestant = function () {
-        ContestantsService.addContestant(angular.copy($scope.newContestant));
-        $scope.newContestant = { lane: '', name: '', score: '' };
-    };
+    // Store a reference to the table row so we can get it again later.
+    htmlForPath[scoreSnapshot.name()] = newScoreRow;
 
-    $scope.updateContestant = function (contestant) {
-        ContestantsService.updateContestant(contestant);
-    };
-
-    $scope.removeContestant = function (contestant) {
-        ContestantsService.removeContestant(contestant);
-    };
-
-    $scope.incrementScore = function () {
-        $scope.currentContestant.score = parseInt($scope.currentContestant.score, 10) + 1;
-        $scope.updateContestant($scope.currentContestant);
-    };
-
-    $scope.decrementScore = function () {
-        $scope.currentContestant.score = parseInt($scope.currentContestant.score, 10) - 1;
-        $scope.updateContestant($scope.currentContestant);
-    };
-}]);
-
-app.factory('ContestantsService', ['$firebase', function ($firebase) {
-    var ref = new Firebase('https://box-arcade.firebaseio.com/contestants/');
-    var contestants = $firebase(ref);
-
-    contestants.$on('loaded', function(){
-        // console.log('contestants', contestants);
-    })
-
-    var getContestants = function() {
-        return contestants;
+    // Insert the new score in the appropriate place in the table.
+    if (prevScoreName === null) {
+      $("#leaderboardTable").append(newScoreRow);
     }
-
-    var addContestant = function (contestant) {
-        contestants.$add(contestant);
-    };
-
-    var updateContestant = function (id) {
-        contestants.$save(id);
-    };
-
-    var removeContestant = function (id) {
-        contestants.$remove(id);
-    };
-
-    return {
-        getContestants: getContestants,
-        addContestant: addContestant,
-        updateContestant: updateContestant,
-        removeContestant: removeContestant
+    else {
+      var lowerScoreRow = htmlForPath[prevScoreName];
+      lowerScoreRow.before(newScoreRow);
     }
-}]);
+}
+
+// Helper function to handle a score object being removed; just removes the corresponding table row.
+function handleScoreRemoved(scoreSnapshot) {
+    var removedScoreRow = htmlForPath[scoreSnapshot.name()];
+    removedScoreRow.remove();
+    delete htmlForPath[scoreSnapshot.name()];
+}
+
+// Create a view to only receive callbacks for the last LEADERBOARD_SIZE scores
+var scoreListView = scoreListRef.limit(LEADERBOARD_SIZE);
+
+// Add a callback to handle when a new score is added.
+scoreListView.on('child_added', function (newScoreSnapshot, prevScoreName) {
+    handleScoreAdded(newScoreSnapshot, prevScoreName);
+});
+
+// Add a callback to handle when a score is removed
+scoreListView.on('child_removed', function (oldScoreSnapshot) {
+    handleScoreRemoved(oldScoreSnapshot);
+});
+
+// Add a callback to handle when a score changes or moves positions.
+var changedCallback = function (scoreSnapshot, prevScoreName) {
+    handleScoreRemoved(scoreSnapshot);
+    handleScoreAdded(scoreSnapshot, prevScoreName);
+};
+scoreListView.on('child_moved', changedCallback);
+scoreListView.on('child_changed', changedCallback);
